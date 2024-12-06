7724a61e879590b88568058ea01de75a58daaa1b
Aldo <aldo.choque@tecsup.edu.pe>
2024-12-05 20:40:51 -0500

    listo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 import Header from "./Components/Header/Header";
-import CarruselImg from "./Components/CarruselImg/CarruselImg";
 import Footer from "./Components/Footer/Footer";
 import Carrusel from "./Components/CarruselImg/Carrusel";
 import WhatsAppWidget from "./Components/whatsap/whatsapwidget";
-import AboutSection from "./Components/about/AboutSection";
+
 
 
 export default function Home() {
